4e7b9d44a26e0f4d78880efd3eb50f20e52f218b
Legolas <057@live.com>
2020-09-12 21:45:42 -0700

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by Legolas on 9/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
